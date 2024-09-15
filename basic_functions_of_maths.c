0424605ab9d054ae5009eb3287688dcee79b06d1
Madhav Varshney <madhav.02varshney@gmail.com>
2024-09-15 22:06:49 +0530

    perform_basic_functions_of_maths

--- basic_functions_of_maths.c
+++ basic_functions_of_maths.c
@@ -1,15 +1,15 @@
-// #include<stdio.h>
-// int main(){
-// int a;
-// int b;
-//   printf("enter a no :" );
-//   scanf("%d",&a);
-//   printf("enter a no :" );
-//   scanf("%d",&b);
-//   printf("the first no is %d\n",a+b);
-//   printf("the second no is %d\n",b-a);
-//   printf("the third no is %d\n",a*b);
-//   printf("the fourth no is %d\n",a/b);
-//   printf("the fifth no is %d\n",a%b);
-//   return 0;
-// }
+ #include<stdio.h>
+ int main(){
+ int a;
+ int b;
+   printf("enter a no :" );
+   scanf("%d",&a);
+   printf("enter second no :" );
+   scanf("%d",&b);
+   printf("the first output is %d\n",a+b);
+   printf("the second output is %d\n",b-a);
+   printf("the third output is %d\n",a*b);
+   printf("the fourth output is %d\n",a/b); // it gives the quotient
+   printf("the fifth output is %d\n",a%b);  // it gives the reminder
+   return 0;
+ }

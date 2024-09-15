639834281f64d76db603ef6222c3fa1517d4f3c7
Madhav Varshney <madhav.02varshney@gmail.com>
2024-09-15 22:01:51 +0530

    Add files via upload

---
+++ basic_functions_of_maths.c
@@ -0,0 +1,15 @@
+// #include<stdio.h>
+// int main(){
+// int a;
+// int b;
+//   printf("enter a no :" );
+//   scanf("%d",&a);
+//   printf("enter a no :" );
+//   scanf("%d",&b);
+//   printf("the first no is %d\n",a+b);
+//   printf("the second no is %d\n",b-a);
+//   printf("the third no is %d\n",a*b);
+//   printf("the fourth no is %d\n",a/b);
+//   printf("the fifth no is %d\n",a%b);
+//   return 0;
+// }
